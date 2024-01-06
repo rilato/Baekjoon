e28f6f34ef968f7100c0e7234c4bf2e94bdaf22f
rilato <croco1997@naver.com>
2024-01-06 17:39:24 +0900

    Update ^1697(숨바꼭질).cpp

--- 알고리즘별/Graph/^1697(숨바꼭질).cpp
+++ 알고리즘별/Graph/^1697(숨바꼭질).cpp
@@ -1,65 +1,68 @@
 // dp인줄 알았으나 bfs
 // 최단 경로를 구할 때 bfs를 사용한다
 
-// https://hagisilecoding.tistory.com/20
-
 #include <iostream>
+#include <vector>
 #include <queue>
 
 using namespace std;
 
-int n, k;
-bool visit[100001];
-queue<pair<int, int>> q;
-
-void bfs(int a);
+int N, K;
+queue <pair <int, int>> q;
+bool visited[100001] = { 0, };
 
 int main() {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
 
-    cin >> n >> k;
+    cin >> N >> K;
 
-    bfs(n);
-
-    return 0;
-}
-
-void bfs(int a) {
-    visit[a] = true;
-    q.push(make_pair(a, 0));
+    q.push({ N, 0 });
+    visited[N] = true;
 
     while (!q.empty()) {
-        int x = q.front().first;
+        int now = q.front().first;
         int cnt = q.front().second;
 
         q.pop();
 
-        if (x == k) {
+        if (now == K) {
             cout << cnt;
             break;
         }
 
-        if (x + 1 >= 0 && x + 1 < 100001) {
-            if (!visit[x + 1]) {
-                visit[x + 1] = true;
-                q.push(make_pair(x + 1, cnt + 1));
-            }
+        // N에서 하나씩 뻗어나갈 때 마다, 모든 경우를 체크하기 위해서 else if를 사용하지 않고, if문을 사용한다!
+        if (now - 1 >= 0 && !visited[now - 1]) {
+            q.push({ now - 1, cnt + 1 });
+            visited[now - 1] = true;
+        }
+        if (now + 1 <= 100000 && !visited[now + 1]) {
+            q.push({ now + 1, cnt + 1 });
+            visited[now + 1] = true;
+        }
+        if (now * 2 <= 100000 && !visited[now * 2]) {
+            q.push({ now * 2, cnt + 1 });
+            visited[now * 2] = true;
         }
 
-        if (x - 1 >= 0 && x - 1 < 100001) {
-            if (!visit[x - 1]) {
-                visit[x - 1] = true;
-                q.push(make_pair(x - 1, cnt + 1));
-            }
+        /*
+        * 아래의 코드는, visited[now - 1]을 먼저 검사하기 때문에, out of bound가 발생할 수 있다!
+        * 이를 막기 위해, 위의 코드 처럼, now - 1 >= 0을 먼저 검사한 후, visited[now - 1]을 검사하도록 한다.
+        if (!visited[now - 1] && now - 1 >= 0) {
+            q.push({ now - 1, cnt + 1 });
+            visited[now - 1] = true;
         }
-
-        if (2 * x >= 0 && 2 * x < 100001) {
-            if (!visit[2 * x]) {
-                visit[2 * x] = true;
-                q.push(make_pair(2 * x, cnt + 1));
-            }
+        if (!visited[now + 1] && now + 1 <= 100000) {
+            q.push({ now + 1, cnt + 1 });
+            visited[now + 1] = true;
         }
+        if (!visited[now * 2] && now * 2 <= 100000) {
+            q.push({ now * 2, cnt + 1 });
+            visited[now * 2] = true;
+        }
+        */
     }
+
+    return 0;
 }
