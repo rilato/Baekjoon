2d4c4d619a902d8f88b961fcf0d1c220b7e49cea
rilato <croco1997@naver.com>
2024-01-12 20:14:10 +0900

    Update ^14501(퇴사).cpp

--- 알고리즘별/DP/^14501(퇴사).cpp
+++ 알고리즘별/DP/^14501(퇴사).cpp
@@ -1,17 +1,11 @@
-// https://velog.io/@sj-lee33/%EB%B0%B1%EC%A4%80-14501%EB%B2%88-%ED%87%B4%EC%82%AC-%EC%95%8C%EA%B3%A0%EB%A6%AC%EC%A6%98-c-%ED%92%80%EC%9D%B4
-
-// 스케줄링의 일환 같은 문제로 보인다.
-// 뒤쪽부터 탐색한다.
-
 #include <iostream>
-#include <algorithm>
+#include <vector>
 
 using namespace std;
 
-int N;
-int Ti[16] = { 0, };
-int Pi[16] = { 0, };
-int res[16] = { 0, };	// 배열 인덱스의 뒤쪽부터 계산해 나감. res[1]이 최종적인 Max값
+int N, T, P;
+vector <pair <int, int>> v;
+vector <int> answer;
 
 int main() {
 	ios::sync_with_stdio(false);
@@ -20,25 +14,27 @@ int main() {
 
 	cin >> N;
 
-	for (int i = 1; i <= N; i++) {
-		cin >> Ti[i] >> Pi[i];
+	// N + 1일까지 계산해야하므로 v보다 1개 더 공간을 확보한다.
+	answer.resize(N + 1);
+
+	for (int i = 0; i < N; i++) {
+		cin >> T >> P;
+
+		v.push_back({ T, P });
 	}
 
-	int deadline;
-
-	for (int i = N; i > 0; i--) {
-		deadline = i + Ti[i];
-		if (deadline > N + 1) {
-			// 상담 불가시 이전 최대 값을 그대로 갖다 씀
-			res[i] = res[i + 1];
+	// i는 0일부터 시작하므로, 마지막으로 상담 가능한 날짜는 N - 1이고,
+	// 상담이 가능한 숫자는 N으로 설정한다.
+	for (int i = N - 1; i >= 0; i--) {
+		if (i + v[i].first <= N) {
+			answer[i] = max(answer[i + 1], v[i].second + answer[i + v[i].first]);
 		}
 		else {
-			// 상담 가능, 최대 이익 판별 필요 (해당 날짜를 진행했을 경우와, 그렇지 않았을 경우를 비교)
-			res[i] = max(res[i + 1], res[deadline] + Pi[i]);
+			answer[i] = answer[i + 1];
 		}
 	}
 
-	cout << res[1];
+	cout << answer[0];
 
 	return 0;
 }
