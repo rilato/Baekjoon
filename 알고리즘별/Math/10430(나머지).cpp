e6a2cb725c0cd9bfdef5a8f1be087396b9282ecf
rilato <croco1997@naver.com>
2024-01-16 13:39:58 +0900

    Create 10430(나머지).cpp

---
+++ 알고리즘별/Math/10430(나머지).cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int A, B, C;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> A >> B >> C;
+
+    cout << (A + B) % C << '\n';
+    cout << ((A % C) + (B % C)) % C << '\n';
+    cout << (A * B) % C << '\n';
+    cout << ((A % C) * (B % C)) % C;
+
+    return 0;
+}
