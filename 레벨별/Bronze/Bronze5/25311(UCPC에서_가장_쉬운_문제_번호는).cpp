de4695caa90d604f35ad4315d1b89914df76beb5
Yunsik Lee <croco1997@naver.com>
2024-01-18 12:44:36 +0900

    Create 25311(UCPC에서_가장_쉬운_문제_번호는).cpp

---
+++ 레벨별/Bronze/Bronze5/25311(UCPC에서_가장_쉬운_문제_번호는).cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int num;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> num;
+
+    cout << "A";
+
+    return 0;
+}
