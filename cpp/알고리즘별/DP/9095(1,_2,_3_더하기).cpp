c59bed516b02195765568e1e8be3c4a4b054e64b
rilato <croco1997@naver.com>
2024-02-15 16:36:47 +0900

    Update 9095(1,_2,_3_더하기).cpp

--- cpp/알고리즘별/DP/9095(1,_2,_3_더하기).cpp
+++ cpp/알고리즘별/DP/9095(1,_2,_3_더하기).cpp
@@ -28,3 +28,51 @@ int main() {
 
     return 0;
 }
+
+
+// 3 :
+// 2를 만들 수 있는 경우의 수 + 마지막에 1 더하기 : dp[2][1] ~ dp[2][3]
+// 1을 만들 수 있는 경우의 수 + 마지막에 2 더하기 : dp[1][1] ~ dp[1][3]
+// 0을 만들 수 있는 경우의 수 + 마지막에 3 더하기 : dp[0][1] ~ dp[0][3]
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int T, num;
+vector <vector <int>> dp;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    dp.resize(11, vector <int>(4));
+
+    dp[1][1] = 1;
+    dp[1][2] = 0;
+    dp[1][3] = 0;
+    dp[2][1] = 1;
+    dp[2][2] = 1;
+    dp[2][3] = 0;
+    dp[3][1] = 2;
+    dp[3][2] = 1;
+    dp[3][3] = 1;
+
+    for (int i = 4; i <= 10; i++) {
+        dp[i][1] = dp[i - 1][1] + dp[i - 1][2] + dp[i - 1][3];
+        dp[i][2] = dp[i - 2][1] + dp[i - 2][2] + dp[i - 2][3];
+        dp[i][3] = dp[i - 3][1] + dp[i - 3][2] + dp[i - 3][3];
+    }
+
+    cin >> T;
+
+    while (T--) {
+        cin >> num;
+
+        cout << dp[num][1] + dp[num][2] + dp[num][3] << '\n';
+    }
+
+    return 0;
+}
